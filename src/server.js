46f059c7791c58583ad49b7df61233a7af74b7ad
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-02 19:56:21 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,3 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const listEndpoints = require("express-list-endpoints")
+
+// Routes:
+const usersRouter = require('./routes/users');
+const roomsRouter = require('./routes/rooms')
+server.use('/rooms', roomsRouter);
+server.use('/users', usersRouter);
+
+// Error Handlers: 
+const { notFoundHandler, badRequestHandler, genericErrorHandler } = require("./errorHandlers")
+server.use(badRequestHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
+console.log(listEndpoints(server))
+
+// Connecting with database:
+mongoose
+  .connect("mongodb://localhost:27017/goldmoon", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+     server.listen(port, () =>{
+       console.log(`Hey! Find me on port: ${port}`)
+    })
+   )
+  .catch((err) => console.log(err))
