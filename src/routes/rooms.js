cab25e1bc5c95402890f2fabca065b12792f0a63
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-02 19:55:53 +0200

    Update rooms.js

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const roomsRouter = express.Router()
+const RoomsSchema = require('../models/rooms');
+
 //get All rooms:
 roomsRouter.get('/', async( req, res, next) =>{
    try{
@@ -7,3 +11,38 @@ roomsRouter.get('/', async( req, res, next) =>{
        next(error)
    }
 })
+
+// post a room:
+roomsRouter.post("/", async( req, res, next) =>{
+    try{
+        const newRoom = new RoomsSchema(req.body)
+        const response = await newRoom.save();
+        res.send(response)
+    }catch(error){
+        next(error)
+    }
+})
+
+// update a room:
+roomsRouter.put("/:id", async(req, res, next) => {
+    try{
+        const room = await RoomsSchema.findByIdAndUpdate(req.params.id, req.body)
+        console.log(room);
+        res.send(room);
+    }catch(error){
+        next(error)
+    }
+})
+
+// delete a room:
+roomsRouter.delete("/:id", async(req, res, next) =>{
+    try{
+        const room = await RoomsSchema.findByIdAndDelete(req.params.id)
+        console.log("Deleted!");
+        res.send("Deleted!");
+    } catch(error){
+        next(error)
+    }
+})
+
+module.exports = roomsRouter
